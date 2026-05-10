1814f3f25303b93a2bffb78b77dd21fb8989c17c
Diego <diegoalfa98gmail.com>
2026-05-10 03:50:27 +0200

    refactor: centralize business info and final html cleanup

---
+++ src/lib/constants.ts
@@ -0,0 +1,15 @@
+export const BUSINESS_INFO = {
+  name: "José María Alfaro",
+  phone: "34666749676",
+  email: "jmaj07@gmail.com",
+  calendarUrl: "https://calendar.google.com/calendar/u/0/r/day", // Placeholder
+  social: {
+    whatsapp: "https://wa.me/34666749676",
+  },
+  sites: {
+    fai: "https://fainmo.es",
+    nuevoMilenio: "https://nuevomilenio.es",
+    alquilabien: "https://alquilabien.es",
+    bpi: "https://bpimadrid.es"
+  }
+};
