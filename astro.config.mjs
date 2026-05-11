49b36b9da6029c15022de0bba59d6f138d1afb98
Diego <diegoalfa98gmail.com>
2026-05-12 00:14:05 +0200

    fix: serve Astro assets on GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://diego-alfadev.github.io',
+  base: '/alfaro-partners-web',
   vite: {
     plugins: [tailwindcss()]
   },
